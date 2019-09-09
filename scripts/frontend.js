3b75c09163d2ccc7bd01a5a87bfa91c49318ff1b
Leslie Reyes <l.reyes.ubiqum@gmail.com>
2019-09-09 16:57:45 +0200

    last update

--- scripts/frontend.js
+++ scripts/frontend.js
@@ -8,7 +8,8 @@ const userInfo = {
     userName: "",
     fullName: "",
     bio: "",
-    image: ""
+    image: "",
+    publicRepos: ""
 };
 
 // a new object is declare, later this is going to be filled with objects that have info of the repos
@@ -18,7 +19,7 @@ function search() {
     // first thing is to display the loader while the data is being fetched, so the container that holds the info is hidden
     document.getElementById("loader").style.display = "block";
     document.getElementById("container").style.display = "none";
-
+    // then the error for when a user doesn't exist is set to display none
     document.getElementById("redBox").style.display = "none";
 
     // the container is cleared so when doing a following search the info displayed before disappear
@@ -26,8 +27,8 @@ function search() {
     container.innerHTML = "";
     // to keep the input value => declare a var that holds it
     var user = document.getElementById("value").value;
-    console.log(user);
     // that var is then sent during the fetch to get the repos of the user searched
+
     fetch("https://api.github.com/users/" + user)
         .then(response => {
             if (response.ok) {
@@ -39,12 +40,20 @@ function search() {
         })
         // in that step the object created at the beginning (userInfo) is filled
         .then(json => {
+
             userInfo.userName = json.login;
             userInfo.fullName = json.name;
             userInfo.bio = json.bio;
             userInfo.image = json.avatar_url;
+            userInfo.publicRepos = json.public_repos;
             // and then the function that does fetchs the repos is called
-            fetchRepos();
+            // if the user has repos then the fetchRepos function is called, else a message 'No repos for this user is displayed'
+            if (user.publicRepos != 0) {
+                fetchRepos();
+            } else {
+                var norepos = document.getElementById('repos').style.display = "block";
+                norepos.innerHTML = "No repos for this user";
+            }
         })
         .catch(error => {
             console.log(error);
@@ -68,7 +77,7 @@ function displayError() {
 //that function fetch the repos owned by the user
 function fetchRepos() {
     var user = document.getElementById("value").value;
-    console.log(user);
+
     fetch("https://api.github.com/users/" + user + "/repos?type=owner")
         .then(response => {
             if (response.ok) {
@@ -91,7 +100,7 @@ function fetchRepos() {
                     forks: json[i].forks_count
                 };
             }
-            console.log(objects);
+
             document.getElementById("loader").style.display = "none";
             document.getElementById("container").style.display = "block";
             fillContent();
@@ -103,7 +112,7 @@ function fetchRepos() {
 }
 
 function fillContent() {
-    //
+    //the content of the userInfo is filled
     document.getElementById("avatar").src = userInfo.image;
     document.getElementById("login").innerHTML = userInfo.userName;
     document.getElementById("fullName").innerHTML = userInfo.fullName;
@@ -111,24 +120,15 @@ function fillContent() {
 }
 
 function fillRepos() {
-    //
+    //the content of the repositories is filled
     var container = document.getElementById("repo");
 
     for (var i = 0; i < Object.keys(objects).length; i++) {
         var infoRepo = document.createElement("div");
         infoRepo.setAttribute("class", "inforepo");
         container.appendChild(infoRepo);
-
         infoRepo.innerHTML += `<div>${objects[i].name}</div>`;
-
-        infoRepo.innerHTML += `<div><img src="./css/images/star.png">${objects[i].stars}</div>`;
-
-        infoRepo.innerHTML += `<div><img src="./css/images/fork.png">${objects[i].forks}</div>`;
+        infoRepo.innerHTML += `<div><img src="../style/star.png">${objects[i].stars}</div>`;
+        infoRepo.innerHTML += `<div><img src="../style/fork.png">${objects[i].forks}</div>`;
     }
-}
-
-export default {
-    search,
-    fetchRepos,
-    fetch
 }
